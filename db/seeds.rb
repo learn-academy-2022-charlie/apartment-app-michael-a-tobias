3d3079288e9c8616443f576d51df569d66dfdee2
michael-a-tobias <michael.a.tobias@outlook.com>
2022-07-06 16:29:15 -0700

    Unprotected index card complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user = User.where(email: 'test@example.com').first_or_create(password: '12345678', password_confirmation: '12345678')
+
+apartments_array = [
+    {
+    street: "123 Snoring Ln.",
+    city: "New Foundland",
+    state: "ND",
+    manager: "Peter Petigrew",
+    email: "peter67@yahoo.com", 
+    price: "$250,000", 
+    bedrooms: 1, 
+    bathrooms: 1, 
+    pets: "No",
+    image: "https://thiscatdoesnotexist.com/"
+    },
+    {
+    street: "8750 Poland Dr.",
+    city: "Philadelphia",
+    state: "PA",
+    manager: "Ronald McDonald",
+    email: "rm1955@aol.com", 
+    price: "$740,000", 
+    bedrooms: 3, 
+    bathrooms: 2, 
+    pets: "Mandatory",
+    image: "https://thiscatdoesnotexist.com/"
+    }
+]
+
+apartments_array.each do |apartment_obj|
+    user.apartments.create apartment_obj
+  end
+
+p user.apartments
+
+p 'Apartments were made successfully.'
