a3610041b856c457fa17ed5f51e0d4d4de04530b
michael-a-tobias <michael.a.tobias@outlook.com>
2022-07-07 10:55:59 -0700

    Unprotected show card complete.

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -37,7 +37,18 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/apartmentindex" render={props => <ApartmentIndex apartments={this.state.apartments} />} />
-          <Route path="/apartmentshow" component={ApartmentShow} />
+          {/* Working on SHOW */}
+          {/* <Route path="/apartmentshow" component={ApartmentShow} /> */}
+
+          <Route path="/apartmentshow/:id" render={(props) => {
+            let id = props.match.params.id
+            let apartment = this.state.apartments.find((apartmentSingular) =>
+              apartmentSingular.id === +id)
+            return <ApartmentShow apartment={apartment} />
+          }} />
+
+
+          {/* END WORK ON SHOW */}
           <Route path="/apartmentnew" component={ApartmentNew} />
           <Route path="/apartmentedit" component={ApartmentEdit} />
           <Route component={NotFound} />
